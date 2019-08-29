dc4608b0b1292b685226c972d9d27385cf0eeb1b
CleoZhang <forest-zhang@hotmail.com>
2019-08-29 23:56:21 +0000

    Mission completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,26 @@
 const request = require('request');
 
-const fetchBreedDescription = function(url, breed, callback) {
+const callback = function(error, body) {
+  let err = "";
+  if (error) {
+    console.log('error:', error);
+    err = "error";
+  }
+  if (body === "[]") {
+    console.log("Breed Not Found");
+    err = "error";
+  }
+  return err;
+};
+
+const fetchBreedDescription = function(url, breed) {
   if (breed !== undefined) {
     request(url, (error, response, body) => {
-      if (error) callback(error);
-
-      if (body === "[]") {
-        console.log("Breed Not Found");
-      } else {
-        const data = JSON.parse(body);
-        // more like a 2d mapping with the first entry
-        console.log(data[0]["description"]);
-      }
+      let err = callback(error, body);
+      if (err) return;
+      const data = JSON.parse(body);
+      // more like a 2d mapping with the first entry
+      console.log(data[0]["description"]);
     });
   } else {
     console.log("Breed typed in is not valid");
--- index.js
+++ index.js
@@ -5,14 +5,4 @@ let args = process.argv.slice(2);
 let breed = args[0];
 url = url + breed;
 
-const callback = function(error, body) {
-  if (error) {
-    console.log('error:', error);
-    return;
-  }
-  if (body === "[]") {
-    console.log("Breed Not Found");
-  } 
-};
-
-fetchBreedDescription(url, breed, callback);
+fetchBreedDescription(url, breed);
