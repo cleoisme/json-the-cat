2a785596ed4939a5a0e9dc13029d12c5066bca28
CleoZhang <forest-zhang@hotmail.com>
2019-09-02 21:42:17 +0000

    Eslint all

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,11 +4,11 @@ let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
 // not right - need to refactor
 const fetchBreedDescription = function(breed, callback) {
-    url += breed;
-    request(url, (error, response, body) => {
-      const data = JSON.parse(body);
-      callback(error, data[0]["description"]);
-    });
+  url += breed;
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    callback(error, data[0]["description"]);
+  });
 };
 
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-if (breed !== undefined) {
+if (breedName !== undefined) {
   fetchBreedDescription(breedName, (error, desc) => {
     if (error) {
       console.log('Error fetch details:', error);
@@ -11,6 +11,6 @@ if (breed !== undefined) {
       console.log(desc);
     }
   });
-}else{
+} else {
   console.log("Invalid breed.");
 }
